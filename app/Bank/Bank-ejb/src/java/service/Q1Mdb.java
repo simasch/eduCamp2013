8c4bae5a5f7a9807e8164dcd4ee387a736da3fc2
Simon Martinelli <sm@simas.ch>
2013-06-15 10:57:25 +0100

    work in progress

--- app/Bank/Bank-ejb/src/java/service/Q1Mdb.java
+++ app/Bank/Bank-ejb/src/java/service/Q1Mdb.java
@@ -1,11 +1,15 @@
 package service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.ejb.SessionContext;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 @MessageDriven(mappedName = "jms/q1", activationConfig = {
     @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
@@ -13,11 +17,15 @@ import javax.jms.MessageListener;
 })
 public class Q1Mdb implements MessageListener {
 
-    @Resource
-    private SessionContext sc;
+    private final static Logger LOGGER = Logger.getLogger(Q1Mdb.class.getName());
 
     @Override
     public void onMessage(Message message) {
-        sc.setRollbackOnly();
+        try {
+            TextMessage tm = (TextMessage) message;
+            LOGGER.info(tm.getText());
+        } catch (JMSException ex) {
+            Logger.getLogger(Q1Mdb.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
