47106d781d548c7b7ce1873292faefe5788631ba
Simon Martinelli <sm@simas.ch>
2013-06-15 12:22:18 +0100

    added clearing queue

--- app/Bank/Bank-ejb/src/java/business/TransactionManager.java
+++ app/Bank/Bank-ejb/src/java/business/TransactionManager.java
@@ -2,11 +2,20 @@ package business;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.ejb.SessionContext;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -23,6 +32,10 @@ public class TransactionManager {
     EntityManager em;
     @Resource
     SessionContext sc;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private ConnectionFactory cf;
+    @Resource(mappedName = "jms/clearing")
+    private Queue queue;
 
     public Transaction withdraw(Account account, BigDecimal amount) throws LimitExceedException {
         // Pessimistic lock does not work with Derby embedded
@@ -60,19 +73,38 @@ public class TransactionManager {
     }
 
     public Transaction transfer(String credit, String debit, BigDecimal amount) {
-        Transaction t = new Transaction();
-        t.setAmount(amount);
-        t.setCredit(credit);
-        t.setDebit(debit);
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setCredit(credit);
+        transaction.setDebit(debit);
 
-        em.persist(t);
-        return t;
+        em.persist(transaction);
+        
+        try {
+            sendTransactionToClearing(transaction);
+        } catch (JMSException ex) {
+            Logger.getLogger(TransactionManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
+        return transaction;
     }
 
     public List<Transaction> getTransactions() {
         TypedQuery t = em.createNamedQuery(Customer.getTransactionsByUser, Transaction.class);
         t.setParameter("user", sc.getCallerPrincipal().getName());
         return t.getResultList();
+    }
+
+    private void sendTransactionToClearing(Transaction transaction) throws JMSException {
+        Connection con = cf.createConnection();
+        Session session = con.createSession(true, Session.AUTO_ACKNOWLEDGE);
+        MessageProducer producer = session.createProducer(queue);
+        TextMessage msg = session.createTextMessage(
+                transaction.getDebit() + ","
+                + transaction.getCredit() + ","
+                + transaction.getAmount());
+        producer.send(msg);
+        session.close();
+        con.close();
     }
 }
